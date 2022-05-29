96a965b9b746b5b8d02733bb7870e343adf0853b
Krzysztof Matyjaszczyk <krzysiek.matyjaszczyk@gmail.com>
2022-05-29 13:47:53 +0200

    Initial commit

---
+++ src/main/java/pl/itkurnik/pedalsapi/TwoPedalsApiApplication.java
@@ -0,0 +1,13 @@
+package pl.itkurnik.pedalsapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwoPedalsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TwoPedalsApiApplication.class, args);
+	}
+
+}
